76f6ece110f23400df4fe0c23772f56f7e0e2b05
Gleb <118758307+gl-el@users.noreply.github.com>
2023-02-07 13:25:15 +0300

    feat: add changing backgrounds depending on time and slider buttons

---
+++ js/slider.js
@@ -0,0 +1,36 @@
+const body = document.querySelector('.body');
+const next = document.querySelector('.slide-next');
+const prev = document.querySelector('.slide-prev');
+const min = 1;
+const max = 20;
+let randomNum = getRandomNum();
+
+function getRandomNum() {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setBg() {
+    const img = new Image();
+    const timeOfDay = getTimesOfDay();
+    const bgNum = `${randomNum}`.padStart(2, '0');
+    const link = `https://raw.githubusercontent.com/gl-el/momentum-assets/main/${timeOfDay}/${bgNum}.jpg`;
+    img.src = link;
+    img.addEventListener('load', () => {
+        body.style.backgroundImage = `url('${link}')`;
+    });
+}
+
+setBg()
+
+next.addEventListener('click', getSliderNext);
+function getSliderNext() {
+    (randomNum < max) ? randomNum++ : randomNum = min;
+    setBg();
+}
+
+
+prev.addEventListener('click', getSliderPrev);
+function getSliderPrev() {
+    (randomNum > min) ? randomNum-- : randomNum = max;
+    setBg();
+}
